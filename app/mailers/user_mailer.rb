baef67bad91b8d9ea97d75e61c1df0efc1209e40
John Maya <jcmaya@hotmail.com>
2014-05-04 09:15:51 -0400

    url

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ActionMailer::Base
     @user = user
     @case = caze
     mail(to:@user.email, subject:"The Case has been updated ##{@case.case_identifier}",
-        body: "A new case has been updated",
+        body: "A new case has been updated http://rescue-path.herokuapp.com/cases/#{@case.id}",
         content_type: "text/html")
   end
 end
