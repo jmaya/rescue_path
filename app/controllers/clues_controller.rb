649b6119544d2a960e3b42d5c1904013461a9c67
John Maya <jcmaya@hotmail.com>
2014-05-04 06:10:52 -0400

    added clues to database

--- app/controllers/clues_controller.rb
+++ app/controllers/clues_controller.rb
@@ -4,7 +4,22 @@ class CluesController < ApplicationController
   def new
     @clue = Clue.new
   end
+
   def create
+    @clue = Clue.new(case_params)
+    @clue.user = current_user
+
+    respond_to do |format|
+      if @clue.save
+        format.html { redirect_to root_path, notice: 'The case was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def case_params
+    params.require(:clue).permit(:info,:case_id)
   end
 
 end
